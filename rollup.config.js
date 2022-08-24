4a227254b12d742ecdcbf823787ae4f4ad681dac
Fred Boyle <fred.boyle@pantheon.io>
2022-08-24 16:26:54 -0700

    Clean-up Rollup config
    Signed-off-by: Fred Boyle <fred.boyle@pantheon.io>

--- rollup.config.js
+++ rollup.config.js
@@ -1,36 +1,34 @@
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
-import { terser } from "rollup-plugin-terser";
-import external from "rollup-plugin-peer-deps-external";
-import postcss from "rollup-plugin-postcss";
-import dts from "rollup-plugin-dts";
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
+import external from 'rollup-plugin-peer-deps-external';
+import postcss from 'rollup-plugin-postcss';
+import dts from 'rollup-plugin-dts';
 
-const pkg = require("./package.json");
+const pkg = require('./package.json');
 
 export default [
-  {
-    input: "src/index.ts",
-    output: [
-      { file: pkg.main, format: "cjs", sourcemap: true, name: "react-lib" },
-      { file: pkg.module, format: "esm", sourcemap: true },
-    ],
-    plugins: [
-      external(),
-      resolve(),
-      commonjs(),
-      typescript({ tsconfig: "./tsconfig.json" }),
-      postcss({
-        plugins: [],
-      }),
-      terser(),
-    ],
-  },
+	{
+		input: 'src/index.ts',
+		output: [
+			{ file: pkg.main, format: 'cjs', sourcemap: true, name: 'react-lib' },
+			{ file: pkg.module, format: 'esm', sourcemap: true },
+		],
+		plugins: [
+			external(),
+			resolve(),
+			commonjs(),
+			typescript({ tsconfig: './tsconfig.json' }),
+			postcss(),
+			terser(),
+		],
+	},
 
-  {
-    input: "_dist/esm/index.d.ts",
-    output: [{ file: "_dist/index.d.ts", format: "esm" }],
-    external: [/\.css$/],
-    plugins: [dts()],
-  },
+	{
+		input: '_dist/esm/index.d.ts',
+		output: [{ file: '_dist/index.d.ts', format: 'esm' }],
+		external: [/\.css$/],
+		plugins: [dts()],
+	},
 ];
