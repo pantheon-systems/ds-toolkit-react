3d137590087e4a6c2093c00f9e035960e1bc0e1d
Fred Boyle <fred.boyle@pantheon.io>
2022-08-24 16:26:14 -0700

    Update Storybook preview CSS
    Signed-off-by: Fred Boyle <fred.boyle@pantheon.io>

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,12 +1,12 @@
-import "@pantheon-systems/pds-tokens/build/css/pds-design-tokens.css";
+import '../_dist/css/pantheon-core.css';
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    expanded: true,
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
+	actions: { argTypesRegex: '^on[A-Z].*' },
+	controls: {
+		expanded: true,
+		matchers: {
+			color: /(background|color)$/i,
+			date: /Date$/,
+		},
+	},
 };
