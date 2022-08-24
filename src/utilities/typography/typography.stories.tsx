5157156f4d618a2a9341b7729774e3e3b355a69a
Fred Boyle <fred.boyle@pantheon.io>
2022-08-24 16:27:57 -0700

    Add foundation and utility CSS, stories, and docs
    Signed-off-by: Fred Boyle <fred.boyle@pantheon.io>

---
+++ src/utilities/typography/typography.stories.tsx
@@ -0,0 +1,27 @@
+import Docs from '!!raw-loader!./typography.docs.mdx';
+
+export default {
+	title: 'Utilities/Typography',
+	parameters: {
+		componentSubtitle: 'Utility classes for typography',
+		docs: {
+			description: {
+				component: Docs,
+			},
+		},
+	},
+	argTypes: {},
+};
+
+const Template = ({}) => {
+	return (
+		<>
+			<div class='pds-subheader'>
+				This is a subheader. Unlike headers (H1-H5) it has no inherent
+				hierarchy.
+			</div>
+		</>
+	);
+};
+
+export const Default = Template.bind({});
