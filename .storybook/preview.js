d0e23ea946e9a0fcc86bef89dc60a5cb0d57a85d
M Miller <melissa.miller@pantheon.io>
2022-09-12 14:11:27 -0700

    Update preview.js
    Add Storybook viewports to match Figma size/device examples

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,58 @@
 import '../_dist/css/pantheon-core.css';
 
+// Add viewports for our example device widths.
+const deviceViewports = {
+	phone: {
+		name: 'Phone',
+		styles: {
+			width: '390px',
+			height: '844px',
+		},
+	},
+	phoneLarge: {
+		name: 'Large Phone',
+		styles: {
+			width: '428px',
+			height: '926px',
+		},
+	},
+	tablet: {
+		name: 'Tablet',
+		styles: {
+			width: '768px',
+			height: '1024px',
+		},
+	},
+	tabletLarge: {
+		name: 'Large Tablet',
+		styles: {
+			width: '1024px',
+			height: '1366px',
+		},
+	},
+	desktop: {
+		name: 'Desktop 1200',
+		styles: {
+			width: '1200px',
+			height: '1024px',
+		},
+	},
+	desktopWide: {
+		name: 'Desktop 1440',
+		styles: {
+			width: '1440px',
+			height: '1024px',
+		},
+	},
+	desktopUltraWide: {
+		name: 'Desktop 1600',
+		styles: {
+			width: '1600px',
+			height: '1024px',
+		},
+	},
+};
+
 export const parameters = {
 	actions: { argTypesRegex: '^on[A-Z].*' },
 	controls: {
@@ -8,5 +61,8 @@ export const parameters = {
 			color: /(background|color)$/i,
 			date: /Date$/,
 		},
+	},
+	viewport: {
+		viewports: deviceViewports,
 	},
 };
