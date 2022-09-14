f501696d0569b6f516a0b4b90b79e40aea075cb2
M Miller <melissa.miller@pantheon.io>
2022-09-14 14:27:25 -0700

    Added a way to use args to preview grid compositions

--- src/utilities/grid/grid.stories.tsx
+++ src/utilities/grid/grid.stories.tsx
@@ -10,54 +10,35 @@ export default {
 				component: Docs,
 			},
 		},
-		controls: { disabled: true },
+	},
+	argTypes: {
+		items: {
+			name: 'Grid Items',
+			description:
+				'Sample grid items for prototyping. Edit, add, or remove items within the array to see various combinations. <br/> <br/> The `sm`, `md`, and `lg`  properties indicate how many columns an item should span at each breakpoint.',
+		},
+	},
+	args: {
+		items: [
+			{ sm: 4, md: 2, lg: 4 },
+			{ sm: 4, md: 2, lg: 4 },
+			{ sm: 4, md: 2, lg: 4 },
+		],
 	},
 };
 
-export const TwoItems = ({}) => {
-	return (
-		<div className='pds-grid'>
-			<div className='pds-grid-item pds-grid-item--sm-2 pds-grid-item--md-3 pds-grid-item--lg-6'>
-				Item 1
-			</div>
-			<div className='pds-grid-item pds-grid-item--sm-2 pds-grid-item--md-3 pds-grid-item--lg-6'>
-				Item 2
-			</div>
-		</div>
+const Template = (args: { items: any }) => {
+	const items = args.items;
+	const gridMarkup: JSX.Element[] = [];
+
+	items.forEach(
+		(item: { sm: number; md: number; lg: number }, index: number) => {
+			const classes = `pds-grid-item pds-grid-item--sm-${item.sm} pds-grid-item--md-${item.md} pds-grid-item--lg-${item.lg}`;
+			gridMarkup.push(<div className={classes}>Item {index + 1}</div>);
+		},
 	);
+
+	return <div className='pds-grid'>{gridMarkup}</div>;
 };
 
-export const ThreeItems = ({}) => {
-	return (
-		<div className='pds-grid'>
-			<div className='pds-grid-item pds-grid-item--sm-4 pds-grid-item--md-2 pds-grid-item--lg-4'>
-				Item 1
-			</div>
-			<div className='pds-grid-item pds-grid-item--sm-4 pds-grid-item--md-2 pds-grid-item--lg-4'>
-				Item 2
-			</div>
-			<div className='pds-grid-item pds-grid-item--sm-4 pds-grid-item--md-2 pds-grid-item--lg-4'>
-				Item 3
-			</div>
-		</div>
-	);
-};
-
-export const FourItems = ({}) => {
-	return (
-		<div className='pds-grid'>
-			<div className='pds-grid-item pds-grid-item--sm-4 pds-grid-item--md-3 pds-grid-item--lg-3'>
-				Item 1
-			</div>
-			<div className='pds-grid-item pds-grid-item--sm-4 pds-grid-item--md-3 pds-grid-item--lg-3'>
-				Item 2
-			</div>
-			<div className='pds-grid-item pds-grid-item--sm-4 pds-grid-item--md-3 pds-grid-item--lg-3'>
-				Item 3
-			</div>
-			<div className='pds-grid-item pds-grid-item--sm-4 pds-grid-item--md-3 pds-grid-item--lg-3'>
-				Item 4
-			</div>
-		</div>
-	);
-};
+export const Default = Template.bind({});
