541f9be003522df9c05417fff7c5fe071c8f8daa
Fred Boyle <fred.boyle@pantheon.io>
2022-09-20 09:11:50 -0700

    feat: Add `ComboboxSelect` component
    Signed-off-by: Fred Boyle <fred.boyle@pantheon.io>

---
+++ src/components/ComboboxSelect/combobox-select-sample-data.js
@@ -0,0 +1,111 @@
+export const sampleSelectOptions = [
+	{
+		label: 'Alpha item',
+		value: 'alpha',
+	},
+	{
+		label: 'Beta item',
+		value: 'beta',
+	},
+	{
+		label: 'Gamma item',
+		value: 'gamma',
+	},
+	{
+		label: 'Epsilon item',
+		value: 'epsilon',
+	},
+];
+
+export const sampleSelectOptionsScrolling = [
+	{
+		label: 'Common waterbuck',
+	},
+	{
+		label: 'Red-necked wallaby',
+	},
+	{
+		label: 'Pelican, australian',
+	},
+	{
+		label: 'Goose, greylag',
+	},
+	{
+		label: 'Sugar glider',
+	},
+	{
+		label: 'Bird, red-billed tropic',
+	},
+	{
+		label: 'Currasow (unidentified)',
+	},
+	{
+		label: 'Black vulture',
+	},
+	{
+		label: 'Lion, australian sea',
+	},
+	{
+		label: 'Hen, sage',
+	},
+	{
+		label: 'Francolin, coqui',
+	},
+	{
+		label: 'Bare-faced go away bird',
+	},
+	{
+		label: 'Barbet, crested',
+	},
+	{
+		label: 'Dove, emerald-spotted wood',
+	},
+	{
+		label: 'Wattled crane',
+	},
+	{
+		label: 'Beaver, north american',
+	},
+	{
+		label: 'Monitor lizard (unidentified)',
+	},
+	{
+		label: 'Glider, sugar',
+	},
+	{
+		label: 'Parakeet, rose-ringed',
+	},
+	{
+		label: 'Macaw, scarlet',
+	},
+	{
+		label: 'Golden brush-tailed possum',
+	},
+	{
+		label: 'Snake, carpet',
+	},
+	{
+		label: 'Tiger snake',
+	},
+	{
+		label: 'Booby, blue-faced',
+	},
+	{
+		label: 'Small-toothed palm civet',
+	},
+	{
+		label: 'Wolf, timber',
+	},
+	{
+		label: 'Radiated tortoise',
+	},
+	{
+		label: 'Gecko, barking',
+	},
+	{
+		label: 'Bottle-nose dolphin',
+	},
+	{
+		label: 'Grenadier, common',
+	},
+];
---
+++ src/components/ComboboxSelect/combobox-select.spec.a11y.js
@@ -0,0 +1,8 @@
+const { test, expect } = require('@playwright/test');
+const { gotoFrame, focusViaTab } = require('../../../src/libs/testing/vrt');
+const { a11yTest } = require('../../../src/libs/testing/a11y');
+
+// enable single file parallelism
+test.describe.configure({ mode: 'parallel' });
+
+test.describe('Components/Combobox select-only', () => {});
