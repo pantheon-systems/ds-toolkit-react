df5370a3e4b821c8f22ac0f5bd8eb807fece17b4
Fred Boyle <fred.boyle@pantheon.io>
2022-09-21 14:39:24 -0700

    Add `ComboboxSelect` tests & snapshots
    Signed-off-by: Fred Boyle <fred.boyle@pantheon.io>

---
+++ src/components/ComboboxSelect/combobox-select.spec.e2e.js
@@ -0,0 +1,105 @@
+const { test, expect } = require('@playwright/test');
+const { gotoFrame, focusViaTab } = require('../../libs/testing/vrt');
+
+const {
+	sampleSelectOptions,
+	sampleSelectOptionsScrolling,
+} = require('./combobox-select-sample-data');
+
+const storyID = 'components-combobox-select--combobox-select';
+
+// enable single file parallelism
+test.describe.configure({ mode: 'parallel' });
+
+test.describe('Components/Combobox select', () => {
+	test.describe('Non-scrolling listbox', () => {
+		test('Option selected (via keyboard)', async ({ page, browserName }) => {
+			await gotoFrame(page, storyID);
+
+			await focusViaTab(page, browserName);
+
+			await page.keyboard.press('Enter');
+			await page.keyboard.press('ArrowDown');
+			await page.keyboard.press('ArrowDown');
+
+			// retrieve the intended option's label
+			const targetOptionLabel = sampleSelectOptions[2].label;
+
+			await page.keyboard.press('Enter');
+
+			const combobox = page.locator('.pds-combobox-select [role="combobox"]');
+
+			expect(await combobox.textContent()).toBe(targetOptionLabel);
+		});
+
+		test('Option selected (via mouse)', async ({ page, browserName }) => {
+			await gotoFrame(page, storyID);
+
+			const combobox = page.locator('.pds-combobox-select [role="combobox"]');
+			await combobox.click();
+
+			// get the intended menu item
+			const optionThird = page.locator(
+				'.pds-combobox-select [role="listbox"] [role="option"] >> nth=2',
+			);
+
+			// retrieve the intended option's label
+			const targetOptionLabel = sampleSelectOptions[2].label;
+
+			await optionThird.click();
+
+			expect(await combobox.textContent()).toBe(targetOptionLabel);
+		});
+	});
+
+	test.describe('Scrolling listbox', () => {
+		test('Option selected (via keyboard)', async ({ page, browserName }) => {
+			await gotoFrame(
+				page,
+				storyID,
+				'selectOptions:sampleSelectOptionsScrolling',
+			);
+
+			await focusViaTab(page, browserName);
+
+			await page.keyboard.press('Enter');
+
+			// arrow down 12 times
+			for (let x = 0; x < 12; x++) {
+				await page.keyboard.press('ArrowDown');
+			}
+
+			// retrieve the intended option's label
+			const targetOptionLabel = sampleSelectOptionsScrolling[12].label;
+
+			await page.keyboard.press('Enter');
+
+			const combobox = page.locator('.pds-combobox-select [role="combobox"]');
+
+			expect(await combobox.textContent()).toBe(targetOptionLabel);
+		});
+
+		test('Option selected (via mouse)', async ({ page, browserName }) => {
+			await gotoFrame(
+				page,
+				storyID,
+				'selectOptions:sampleSelectOptionsScrolling',
+			);
+
+			const combobox = page.locator('.pds-combobox-select [role="combobox"]');
+			await combobox.click();
+
+			// get the intended menu item
+			const optionThird = page.locator(
+				'.pds-combobox-select [role="listbox"] [role="option"] >> nth=12',
+			);
+
+			// retrieve the intended option's label
+			const targetOptionLabel = sampleSelectOptionsScrolling[12].label;
+
+			await optionThird.click();
+
+			expect(await combobox.textContent()).toBe(targetOptionLabel);
+		});
+	});
+});
