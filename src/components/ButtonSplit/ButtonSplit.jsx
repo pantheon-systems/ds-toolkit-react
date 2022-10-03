aeb5413b67f828a6f4b6995fe9ab2a140d5deb97
Fred Boyle <fred.boyle@pantheon.io>
2022-10-03 10:45:26 -0700

    fix(ButtonSplit): Change initial listbox positioning & update snapshots
    Signed-off-by: Fred Boyle <fred.boyle@pantheon.io>

--- src/components/ButtonSplit/ButtonSplit.jsx
+++ src/components/ButtonSplit/ButtonSplit.jsx
@@ -24,6 +24,7 @@ const ButtonSplit = ({ actionItems }) => {
 			<MenuButton
 				menuItems={actionItems}
 				className='pds-button-split__menu-button'
+				listboxPositionInitial='bottom-end'
 			/>
 		</span>
 	);
