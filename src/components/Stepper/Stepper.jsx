b18cab8c86a82c3622cbe74c708e1ba66f1551cc
M Miller <melissa.miller@pantheon.io>
2022-10-03 17:34:49 -0700

    Begin stepper styling

--- src/components/Stepper/Stepper.jsx
+++ src/components/Stepper/Stepper.jsx
@@ -10,23 +10,35 @@ const Stepper = ({}) => {
 		<div aria-label='progress' className='pds-stepper' data-steps='4'>
 			<ol className='pds-stepper__steps'>
 				<li className='pds-stepper__step pds-stepper__step--complete'>
-					Step 1 Label
-					<span className='sr-only'>Step 1 of 4, completed</span>
+					<div className='pds-stepper__step-indicator'>1</div>
+					<div className='pds-stepper__step-label'>
+						Step 1 Label
+						<span className='sr-only'>Step 1 of 4, completed</span>
+					</div>
 				</li>
 				<li
-					className='pds-stepper__step pds-stepper__step--active'
+					className='pds-stepper__step pds-stepper__step--current'
 					aria-current='step'
 				>
-					Step 2 Label
-					<span className='sr-only'>Step 2 of 4</span>
+					<div className='pds-stepper__step-indicator'>2</div>
+					<div className='pds-stepper__step-label'>
+						Step 2 Label
+						<span className='sr-only'>Step 2 of 4</span>
+					</div>
 				</li>
 				<li className='pds-stepper__step'>
-					Step 3 Label
-					<span className='sr-only'>Step 3 of 4, not completed</span>
+					<div className='pds-stepper__step-indicator'>3</div>
+					<div className='pds-stepper__step-label'>
+						Step 3 Label
+						<span className='sr-only'>Step 3 of 4, not completed</span>
+					</div>
 				</li>
 				<li className='pds-stepper__step'>
-					Step 4 Label
-					<span className='sr-only'>Step 4 of 4, not completed</span>
+					<div className='pds-stepper__step-indicator'>4</div>
+					<div className='pds-stepper__step-label'>
+						Step 4 Label
+						<span className='sr-only'>Step 4 of 4, not completed</span>
+					</div>
 				</li>
 			</ol>
 		</div>
