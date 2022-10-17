45f54e5904ae83fe33fd48e16061f0485c910eeb
M Miller <melissa.miller@pantheon.io>
2022-10-16 17:47:04 -0700

    Add VR tests

--- src/components/Stepper/Stepper.spec.vrt.js
+++ src/components/Stepper/Stepper.spec.vrt.js
@@ -1,21 +1,64 @@
 const { test, expect } = require('@playwright/test');
 const { gotoFrame, focusViaTab } = require('../../libs/testing/vrt');
 
-const storyID = 'components-stepper--';
+const storyID = 'components-stepper--stepper';
 
 // enable single file parallelism
 test.describe.configure({ mode: 'parallel' });
 
 // Page screenshot clipping/cropping dimensions
-const clipOptions = { x: 0, y: 0, width: 350, height: 500 };
+const clipOptions = { x: 0, y: 0, width: 1200, height: 300 };
 
 test.describe('Components/Stepper', () => {
-	test.describe('General <RENAME IF APPROPRIATE>', () => {
-		test('Base <RENAME IF APPROPRIATE>', async ({ page }) => {
+	test.describe('General', () => {
+		test('Base', async ({ page }) => {
 			await gotoFrame(page, storyID);
 
 			expect(await page.screenshot({ clip: clipOptions })).toMatchSnapshot(
 				'stepper__general-base.png',
+			);
+		});
+
+		test('State: hover', async ({ page }) => {
+			await gotoFrame(page, storyID);
+
+			const button = page.locator('button.pds-stepper__step-content');
+
+			await button.hover();
+
+			expect(await page.screenshot({ clip: clipOptions })).toMatchSnapshot(
+				'stepper__general-hover.png',
+			);
+		});
+
+		test('State: focused', async ({ page, browserName }) => {
+			await gotoFrame(page, storyID);
+
+			await focusViaTab(page, browserName);
+
+			expect(await page.screenshot({ clip: clipOptions })).toMatchSnapshot(
+				'stepper__general-focused.png',
+			);
+		});
+
+		test('State: active', async ({ page }) => {
+			await gotoFrame(page, storyID);
+
+			const button = page.locator('button.pds-stepper__step-content');
+			await button.hover();
+			await page.mouse.down();
+
+			expect(await page.screenshot({ clip: clipOptions })).toMatchSnapshot(
+				'stepper__general-active.png',
+			);
+		});
+	});
+	test.describe('With Error', () => {
+		test('Base', async ({ page }) => {
+			await gotoFrame(page, storyID, 'steps:threeStepsWithError');
+
+			expect(await page.screenshot({ clip: clipOptions })).toMatchSnapshot(
+				'stepper__with-error-base.png',
 			);
 		});
 	});
