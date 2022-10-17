90627289f4293c44c08cc4cff8f4b00609aed316
M Miller <melissa.miller@pantheon.io>
2022-10-16 18:16:31 -0700

    Update Stepper.spec.a11y.js
    Write a11y tests

--- src/components/Stepper/Stepper.spec.a11y.js
+++ src/components/Stepper/Stepper.spec.a11y.js
@@ -1,24 +1,146 @@
-/*
- * !!! DELETE THIS FILE
- * -> if no specific accessibility/a11y tests are needed for the component
- */
-
 const { test, expect } = require('@playwright/test');
 const { gotoFrame, focusViaTab } = require('../../../src/libs/testing/vrt');
 const { a11yTest } = require('../../../src/libs/testing/a11y');
 
-const storyID = 'components-stepper--';
+const storyID = 'components-stepper--stepper';
 
 // enable single file parallelism
 test.describe.configure({ mode: 'parallel' });
 
 test.describe('Components/Stepper', () => {
-	test('<PROVIDE DESCRIPTIVE NAME HERE>', async ({ page }) => {
+	test('Base', async ({ page }) => {
 		await gotoFrame(page, storyID);
 
 		const a11yNumViolations = await a11yTest(
 			page,
-			'stepper__default',
+			'components-stepper__default',
+		);
+
+		expect(
+			a11yNumViolations,
+			`${a11yNumViolations} accessibility issues found.`,
+		).toBe(0);
+	});
+
+	test('State: hover', async ({ page }) => {
+		await gotoFrame(page, storyID);
+
+		const button = page.locator('button.pds-stepper__step-content');
+
+		await button.hover();
+
+		const a11yNumViolations = await a11yTest(
+			page,
+			'components-stepper__default-hover',
+		);
+
+		expect(
+			a11yNumViolations,
+			`${a11yNumViolations} accessibility issues found.`,
+		).toBe(0);
+	});
+
+	test('State: focused', async ({ page, browserName }) => {
+		await gotoFrame(page, storyID);
+
+		await focusViaTab(page, browserName);
+
+		const a11yNumViolations = await a11yTest(
+			page,
+			'components-stepper__default-focused',
+		);
+
+		expect(
+			a11yNumViolations,
+			`${a11yNumViolations} accessibility issues found.`,
+		).toBe(0);
+	});
+
+	test('State: active', async ({ page, browserName }) => {
+		await gotoFrame(page, storyID);
+
+		const button = page.locator('button.pds-stepper__step-content');
+
+		await button.hover();
+		await page.mouse.down();
+
+		const a11yNumViolations = await a11yTest(
+			page,
+			'components-stepper__default-active',
+		);
+
+		expect(
+			a11yNumViolations,
+			`${a11yNumViolations} accessibility issues found.`,
+		).toBe(0);
+	});
+
+	test('Activate button (via mouse) ', async ({ page }) => {
+		await gotoFrame(page, storyID);
+
+		const button = page.locator('button.pds-stepper__step-content');
+
+		await button.click();
+
+		const a11yNumViolations = await a11yTest(
+			page,
+			'components-stepper__default-activate-button-mouse',
+		);
+
+		expect(
+			a11yNumViolations,
+			`${a11yNumViolations} accessibility issues found.`,
+		).toBe(0);
+	});
+
+	test('Activate button (via keyboard: enter) ', async ({
+		page,
+		browserName,
+	}) => {
+		await gotoFrame(page, storyID);
+
+		await focusViaTab(page, browserName);
+
+		await page.keyboard.press('Enter');
+
+		const a11yNumViolations = await a11yTest(
+			page,
+			'components-stepper__default-activate-keyboard-enter',
+		);
+
+		expect(
+			a11yNumViolations,
+			`${a11yNumViolations} accessibility issues found.`,
+		).toBe(0);
+	});
+
+	test('Activate button (via keyboard: space) ', async ({
+		page,
+		browserName,
+	}) => {
+		await gotoFrame(page, storyID);
+
+		await focusViaTab(page, browserName);
+
+		await page.keyboard.press('Space');
+
+		const a11yNumViolations = await a11yTest(
+			page,
+			'components-stepper__default-activate-keyboard-space',
+		);
+
+		expect(
+			a11yNumViolations,
+			`${a11yNumViolations} accessibility issues found.`,
+		).toBe(0);
+	});
+
+	test('Step with Error', async ({ page }) => {
+		await gotoFrame(page, storyID, 'steps:threeStepsWithError');
+
+		const a11yNumViolations = await a11yTest(
+			page,
+			'components-stepper__step-with-error',
 		);
 
 		expect(
