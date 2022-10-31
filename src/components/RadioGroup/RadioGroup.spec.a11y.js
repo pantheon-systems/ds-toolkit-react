10ebfa6c54487f7151594d6243472301627939da
Fred Boyle <fred.boyle@pantheon.io>
2022-10-31 10:28:29 -0700

    feat(Radio Group): Add tests & snapshots
    Signed-off-by: Fred Boyle <fred.boyle@pantheon.io>

--- src/components/RadioGroup/RadioGroup.spec.a11y.js
+++ src/components/RadioGroup/RadioGroup.spec.a11y.js
@@ -1,8 +1,3 @@
-/*
- * !!! DELETE THIS FILE
- * -> if no specific accessibility/a11y tests are needed for the component
- */
-
 const { test, expect } = require('@playwright/test');
 const { gotoFrame, focusViaTab } = require('../../../src/libs/testing/vrt');
 const { a11yTest } = require('../../../src/libs/testing/a11y');
@@ -12,11 +7,32 @@ const storyID = 'components-inputs-radio-group--radio-group';
 // enable single file parallelism
 test.describe.configure({ mode: 'parallel' });
 
-test.describe('Components/Radio Group', () => {
-	test('<PROVIDE DESCRIPTIVE NAME HERE>', async ({ page }) => {
+test.describe('Components/Inputs/Radio Group', () => {
+	test('Default', async ({ page }) => {
 		await gotoFrame(page, storyID);
 
 		const a11yNumViolations = await a11yTest(page, 'radio-group__default');
+
+		expect(
+			a11yNumViolations,
+			`${a11yNumViolations} accessibility issues found.`,
+		).toBe(0);
+	});
+
+	test('Selected', async ({ page }) => {
+		await gotoFrame(page, storyID);
+
+		// get the intended option
+		const optionSecond = page.locator(
+			'.pds-radio-group__options .pds-radio-group__option >> nth=1',
+		);
+
+		await optionSecond.click();
+
+		// wait for animation to complete
+		await page.waitForTimeout(250);
+
+		const a11yNumViolations = await a11yTest(page, 'radio-group__selected');
 
 		expect(
 			a11yNumViolations,
