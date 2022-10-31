6959b7616c7e0347d58bd9e8f0262923c515fd4a
Fred Boyle <fred.boyle@pantheon.io>
2022-10-31 10:28:29 -0700

    feat(Radio Group): Add tests & snapshots
    Signed-off-by: Fred Boyle <fred.boyle@pantheon.io>

--- src/components/RadioGroup/RadioGroup.spec.vrt.js
+++ src/components/RadioGroup/RadioGroup.spec.vrt.js
@@ -9,14 +9,45 @@ test.describe.configure({ mode: 'parallel' });
 // Page screenshot clipping/cropping dimensions
 const clipOptions = { x: 0, y: 0, width: 350, height: 500 };
 
-test.describe('Components/Radio Group', () => {
-	test.describe('General <RENAME IF APPROPRIATE>', () => {
-		test('Base <RENAME IF APPROPRIATE>', async ({ page }) => {
-			await gotoFrame(page, storyID);
+test.describe('Components/Inputs/Radio Group', () => {
+	test('Default', async ({ page }) => {
+		await gotoFrame(page, storyID);
 
-			expect(await page.screenshot({ clip: clipOptions })).toMatchSnapshot(
-				'radio-group__general-base.png',
-			);
-		});
+		expect(await page.screenshot({ clip: clipOptions })).toMatchSnapshot(
+			'radio-group__default.png',
+		);
+	});
+
+	test('Select option: via mouse', async ({ page }) => {
+		await gotoFrame(page, storyID);
+
+		// get the intended option
+		const optionSecond = page.locator(
+			'.pds-radio-group__options .pds-radio-group__option >> nth=1',
+		);
+
+		await optionSecond.click();
+
+		// wait for animation to complete
+		await page.waitForTimeout(250);
+
+		expect(await page.screenshot({ clip: clipOptions })).toMatchSnapshot(
+			'radio-group__selection-via-mouse.png',
+		);
+	});
+
+	test('Select option: via keyboard', async ({ page, browserName }) => {
+		await gotoFrame(page, storyID);
+
+		await focusViaTab(page, browserName);
+
+		await page.keyboard.press('ArrowDown');
+
+		// wait for animation to complete
+		await page.waitForTimeout(250);
+
+		expect(await page.screenshot({ clip: clipOptions })).toMatchSnapshot(
+			'radio-group__selection-via-keyboard.png',
+		);
 	});
 });
