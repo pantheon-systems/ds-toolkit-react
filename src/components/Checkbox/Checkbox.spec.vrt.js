2a697d77ba785fc918b08d7afc6503702641fd4c
M Miller <melissa.miller@pantheon.io>
2022-11-09 20:46:09 -0800

    Add vr tests

--- src/components/Checkbox/Checkbox.spec.vrt.js
+++ src/components/Checkbox/Checkbox.spec.vrt.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 const { gotoFrame, focusViaTab } = require('../../libs/testing/vrt');
 
-const storyID = 'components-checkbox--';
+const storyID = 'components-inputs-checkbox--checkbox';
 
 // enable single file parallelism
 test.describe.configure({ mode: 'parallel' });
@@ -9,14 +9,36 @@ test.describe.configure({ mode: 'parallel' });
 // Page screenshot clipping/cropping dimensions
 const clipOptions = { x: 0, y: 0, width: 350, height: 500 };
 
-test.describe('Components/Checkbox', () => {
-	test.describe('General <RENAME IF APPROPRIATE>', () => {
-		test('Base <RENAME IF APPROPRIATE>', async ({ page }) => {
-			await gotoFrame(page, storyID);
+test.describe('Components/Inputs/Checkbox', () => {
+	test('Default', async ({ page }) => {
+		await gotoFrame(page, storyID);
 
-			expect(await page.screenshot({ clip: clipOptions })).toMatchSnapshot(
-				'checkbox__general-base.png',
-			);
-		});
+		expect(await page.screenshot({ clip: clipOptions })).toMatchSnapshot(
+			'checkbox__default.png',
+		);
+	});
+
+	test('Select checkbox: via mouse', async ({ page }) => {
+		await gotoFrame(page, storyID);
+
+		const checkbox = page.locator('#pds-sample-checkbox');
+
+		await checkbox.click();
+
+		expect(await page.screenshot({ clip: clipOptions })).toMatchSnapshot(
+			'checkbox__select-via-mouse.png',
+		);
+	});
+
+	test('Select checkbox: via keyboard', async ({ page, browserName }) => {
+		await gotoFrame(page, storyID);
+
+		await focusViaTab(page, browserName);
+
+		await page.keyboard.press('Space');
+
+		expect(await page.screenshot({ clip: clipOptions })).toMatchSnapshot(
+			'checkbox__select-via-keyboard.png',
+		);
 	});
 });
--- src/components/CheckboxGroup/CheckboxGroup.spec.vrt.js
+++ src/components/CheckboxGroup/CheckboxGroup.spec.vrt.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 const { gotoFrame, focusViaTab } = require('../../libs/testing/vrt');
 
-const storyID = 'components-checkbox-group--';
+const storyID = 'components-inputs-checkbox-group--checkbox-group';
 
 // enable single file parallelism
 test.describe.configure({ mode: 'parallel' });
@@ -9,14 +9,38 @@ test.describe.configure({ mode: 'parallel' });
 // Page screenshot clipping/cropping dimensions
 const clipOptions = { x: 0, y: 0, width: 350, height: 500 };
 
-test.describe('Components/Checkbox Group', () => {
-	test.describe('General <RENAME IF APPROPRIATE>', () => {
-		test('Base <RENAME IF APPROPRIATE>', async ({ page }) => {
-			await gotoFrame(page, storyID);
+test.describe('Components/Inputs/Checkbox Group', () => {
+	test('Default', async ({ page }) => {
+		await gotoFrame(page, storyID);
 
-			expect(await page.screenshot({ clip: clipOptions })).toMatchSnapshot(
-				'checkbox-group__general-base.png',
-			);
-		});
+		expect(await page.screenshot({ clip: clipOptions })).toMatchSnapshot(
+			'checkbox-group__default.png',
+		);
+	});
+
+	test('Select option: via mouse', async ({ page }) => {
+		await gotoFrame(page, storyID);
+
+		const optionSecond = page.locator('#pds-sample-checkbox-group-option-1');
+
+		await optionSecond.click();
+
+		expect(await page.screenshot({ clip: clipOptions })).toMatchSnapshot(
+			'checkbox-group__selection-via-mouse.png',
+		);
+	});
+
+	test('Select option: via keyboard', async ({ page, browserName }) => {
+		await gotoFrame(page, storyID);
+
+		await focusViaTab(page, browserName);
+
+		await focusViaTab(page, browserName);
+
+		await page.keyboard.press('Space');
+
+		expect(await page.screenshot({ clip: clipOptions })).toMatchSnapshot(
+			'checkbox-group__selection-via-keyboard.png',
+		);
 	});
 });
